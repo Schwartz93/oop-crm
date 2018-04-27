048cb2302b169de05212996c6cef2000ea4123df
Michael Schwartz <michael.schwartz93@hotmail.com>
2018-04-27 13:26:02 +0200

    Login & Register FERTIG

--- classes/config.php
+++ classes/config.php
@@ -1,13 +1,40 @@
 <?php 
-// SETTING $PATH TO NULL TO CHECK IF $PATH EXISTS
+/**
+ *  Config Klasse: 
+ *  
+ *  Statische, öffentliche Funktion "get" wird definiert.
+ *  Innerhalb der Klammern wird $path erstellt und gleich "null" gesetzt.
+ *  $path wird anschließend mit "if" auf das Vorhandensein eines Werts überprüft.
+ *  Ist das Ergebnis dieser if Abfrage true ($path enthält einen Wert), wird das "config" Array in einer Variable $config gespeichert.
+ *  Mit "explode" wird ein Array von dem string $path (zb. ('mysql/host')) zurückgegeben und neu in der Variable $path gespeichert.
+ *  Dieses Array wird mit der foreach Schleife durchlaufen. In $value werden die Werte des Arrays gespeichert.
+ *  Mit "isset" wird überprüft ob die in $value gespeicherten Werte auch im "$GLOBALS" Array existieren.
+ *  Existieren sie,werden sie einzeln in die $config Variable gespeichert und anschließend zrückgegeben.
+ *  $config Wird bei jedem Schleifendurchlauf neu zugewiesen, da zu Beginn nur "mysql" teil des "config" Arrays ist. "host" ist Teil des "mysql" Arrays.
+ *   Daher muss $config im nächsten Durchlauf auf "mysql" gesetzt werden.
+ * 
+ *  Wird $path kein Wert übergeben wird die Funktion mit return false beendet.
+ * 
+ *  
+ *  Ziel: Output im Stil von: echo Config::get('mysql/host'); // 127.0.0.1
+ *  Kann für PDO verwendet werden. 
+ *  Vorteil: Sollte sich die Server/DB Config ändern, muss das im Code nicht mehr berücksichtigt werden.
+ * 
+ */
     class Config {
         public static function get($path = null) {
             if($path) {
                 $config = $GLOBALS['config'];
                 $path = explode('/', $path);
 
-                print_r($path);
+                foreach ($path as $value) {
+                    if (isset($config[$value])) {
+                        $config = $config[$value];
+                    }
+                }
+                return $config;
             }
+            return false;
         }
     }
 ?>
--- core/init.php
+++ core/init.php
@@ -1,7 +1,7 @@
 <?php 
-// SESSION START
+// Session wird gestartet
     session_start();
-// SET UP CONNECTION DETAILS FOR THE DATABASE
+// Im $GLOBALS Array werden die DB Werte mit hilfe von assoziativen Arrays gespeichert.
     $GLOBALS['config'] = array(
         'mysql' => array(
             'host' => '127.0.0.1',
@@ -9,21 +9,42 @@
             'password' => 'root',
             'db' => 'crm'
         ),
-// SET UP "REMEMBER ME" DETAILS
+// "Remember Me" mit Cookie Daten.
         'remember' => array(
             'cookie_name' => 'hash',
             'cookie_expiry' => 604800
         ),
-// SET UP SESSION DETAILS
+// Session Details.
         'session' => array(
-            'session_name' => 'user'
+            'session_name' => 'user',
+            'token_name' => 'token'
         )
     );
-// AUTOLOADING CLASSES. $ CLASS IS BEING SUBSTITUTED WITH WHATEVER CLASS IS IN USE
+/**
+ * spl_autoload_register (spl = Standard PHP Library): 
+ * 
+ * Lädt automatisch Klassen nach.
+ * Erwartet Parameter -> Name der Funktion die eine Klasse laden soll. Hier: "$class".
+ * $class steht für die Klassennamen im Ordner "classes".
+ * 
+ * Vorteil: PHP Datein müssen nicht per Hand reingeladen werden.
+ * 
+ * 
+ */
     spl_autoload_register(function($class) {
         require_once 'classes/' . $class . '.php';
     });
 
     require_once 'functions/sanitize.php';
+
+    if(Cookie::exists(Config::get('remember/cookie_name')) && !Session::exists(Config::get('session/session_name'))) {
+        $hash = Cookie::get(Config::get('remember/cookie_name'));
+        $hashCheck = DB::getInstance()->get('users_session', array('hash', '=', $hash));
+
+        if($hashCheck->count()) {
+            $user = new User($hashCheck->first()->user_id);
+            $user->login();
+        }
+    }
 
 ?>
