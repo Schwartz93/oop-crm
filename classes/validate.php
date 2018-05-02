e86883ae645d9f11cce6786957f129677bdb0a7c
Michael Schwartz <michael.schwartz93@hotmail.com>
2018-05-02 13:44:10 +0200

    Funktionen Beschreibung

--- classes/validate.php
+++ classes/validate.php
@@ -1,4 +1,30 @@
 <?php
+
+/**
+ * Klasse Validate
+ * 
+ * Eigenschaften: $_passed, $_errors, $_db;
+ * 
+ * Methode: __construct() => Aufgerufen wenn die Validate Klasse instanziert wurde. Die Instanz wird in $_db gespeichert.
+ * 
+ * Methode: check() => Erwartet eine "Quelle" hier $_POST und ein Array mit den Rules.
+ * 
+ * Mit der ersten foreach Schleife das $items array durchlaufen => 'username', 'password', 'password_again' und 'name'.
+ * $rules entspricht den verschachtelten arrays innerhalb von 'username', 'password', etc. Also: 'require', 'min' ....
+ * 
+ * Mit der zweiten foreach Schleife durch $rules laufen und die values als $rule_value definieren.
+ * 
+ * Es wird mit if überprüft ob $rule === 'required' und ob $value leer ist. => Error
+ * Ist $value nicht leer wird mit switch case geprüft ob die eingetragenen Werte den $rules entsprechen.
+ * 
+ * Beispiel: if (strlen($value) < $rule_value) {
+                                $this->addError("{$item} must be a minimum of {$rule_value} characters.");
+                            }
+ * 
+ * 
+ * 
+ */
+
 class Validate {
     private $_passed = false,
             $_errors = array(),
@@ -52,13 +78,31 @@ class Validate {
         return $this;
     }
 
+    /**
+     * Methode addError()
+     * 
+     * Fügt einen Error zum errors array hinzu
+     */
+
     private function addError($error) {
         $this->_errors[] = $error;
     }
 
+    /**
+     * Methode errors()
+     * 
+     * Gibt error zurück
+     */
+
     public function errors() {
         return $this->_errors;
     }
+
+    /**
+     * Methode passed()
+     * 
+     * Gibt $_passed zurück. Per default false, wenn keine Fehler vorhanden => true; 
+     */
 
     public function passed() {
         return $this->_passed;
