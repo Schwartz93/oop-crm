10e1cef8b34d777d75f50dbd7eb021cbd7a41a8b
Michael Schwartz <michael.schwartz93@hotmail.com>
2018-05-07 23:56:29 +0200

    login & not logged in css

--- index.php
+++ index.php
@@ -10,28 +10,44 @@
   // Ist der User eingelogged wird sein Name ausgegeben. Sowie die Möglichkeit zum Ausloggen, Ändern der eignene Daten bzw Ändern des Passworts.
   if($user->isLoggedIn()) {
   ?>  
-      <p>Hello <a href="profile.php?user=<?php echo escape($user->data()->username); ?>"><?php echo escape($user->data()->username); ?></a>!</p>
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <link rel="stylesheet" href="main.css">
+    
+    <title>Hallo</title>
+  </head>
+  <body>
+    <p>Hello <a href="profile.php?user=<?php echo escape($user->data()->username); ?>"><?php echo escape($user->data()->username); ?></a>!</p>
 
-        <ul>
-            <li><a href="logout.php">Log out</a></li>
-            <li><a href="update.php">Update Details</a></li>
-            <li><a href="changepassword.php">Change Password</a></li>
-            <li><a href="todo/index.php">Todo App</a></li>
-            <li><a href="cms/admin/list.php">CMS/Blog</a></li>
-            <li><a href="poll/index.php">Polls</a></li>
-            <li><a href="timetracking/index.php">Timetracker</a></li>
-        </ul>
+      <ul>
+          <li><a href="logout.php">Log out</a></li>
+          <li><a href="update.php">Update Details</a></li>
+          <li><a href="changepassword.php">Change Password</a></li>
+          <li><a href="todo/index.php">Todo App</a></li>
+          <li><a href="cms/admin/list.php">CMS/Blog</a></li>
+          <li><a href="poll/index.php">Polls</a></li>
+          <li><a href="timetracking/index.php">Timetracker</a></li>
+      </ul>
 
-  <?php
+<?php
 // Ist der User nicht eingelogged, bekommt er die Möglichkeit dazu. Auch ein link zum registrieren wird angezeigt.
 } else {
-      echo '<p>You need to <a href="login.php">log in</a> or <a href="register.php">register</a></p>';
-  }
-
-  // Hat der User den Admin Status wird es angezeigt.
-  if($user->hasPermission('admin')) {
-    echo '<p> Admin! </p>';
+    header('Location: notLoggedIn.php');
 }
 
-  ?>
-  
+// Hat der User den Admin Status wird es angezeigt.
+if($user->hasPermission('admin')) {
+echo '<p> Admin! </p>';
+}
+
+?>
+  </body>
+  </html>
--- login.php
+++ login.php
@@ -33,28 +33,56 @@
         }
     }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+  <link rel="stylesheet" href="authentication_css/login.css">
 
-<form action="" method="post">
-    <div class="field">
-        <label for="username">Username</label>
-        <input type="text" name="username" id="username" autocomplete="off">
+  <title>Login</title>
+</head>
+<body>
+<div class="jumbotron">
+    <h1 class="display-5">Login</h1>
+        <p class="lead">
+            Please put in your username and your password.<br>
+            If you need to register first, there is a link for that too!
+        </p>
+  </div>
+    <div class="underline"></div>
+    <div class="form-wrapper">
+        <form action="" method="post">
+            <div class="form-group">
+                <div class="field">
+                    <label for="username">Username</label>
+                    <input class="form-control" type="text" name="username" id="username" autocomplete="off">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="field">
+                    <label for="password">Password</label>
+                    <input class="form-control" type="password" name="password" id="password" autocomplete="off">
+                </div>
+            </div>
+                <div class="field">
+                    <label for="remember">
+                        <input type="checkbox" name="remember" id="remember"> Remember Me
+                    </label>
+                </div>
+            <div class="form-group">
+                <div class="field">
+                    <a href="register.php"> Need an account? Click Here</a>
+                </div>
+            </div>  
+                <input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
+                <input class="btn btn-primary" type="submit" value="Log in">
+        </form>
     </div>
-
-    <div class="field">
-        <label for="password">Password</label>
-        <input type="password" name="password" id="password" autocomplete="off">
-    </div>
-
-    <div class="field">
-        <label for="remember">
-            <input type="checkbox" name="remember" id="remember"> Remember Me
-        </label>
-    </div>
-
-    <div class="field">
-        <a href="register.php"> Need an account? Click Here</a>
-    </div>
-
-    <input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
-    <input type="submit" value="Log in">
-</form>
+</body>
+</html>
