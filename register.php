02f387208022d340943f8ba98094cd78dca4c194
Michael Schwartz <michael.schwartz93@hotmail.com>
2018-05-08 10:16:32 +0200

    register.css

--- register.php
+++ register.php
@@ -61,28 +61,68 @@ require_once 'core/init.php';
         }
     }
 ?>
-<form action="" method="post">
-    <div class="field">
-        <label for="username">Username</label>
-        <input type="text" name="username" id="username" value="<?php echo escape(Input::get('username')); ?>" autocomplete="off">
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+  <link rel="stylesheet" href="authentication_css/register.css">
+
+  <title>Login</title>
+</head>
+<body>
+<div class="jumbotron">
+    <h1 class="display-5">Register</h1>
+        <p class="lead">
+            Please choose your Username and password.<br>
+            After re-entering your password,
+            type in you full name.
+        </p>
+  </div>
+  <div class="underline"></div>
+  <div class="form-wrapper">
+        <form action="" method="post">
+        <div class="form-group">
+            <div class="field">
+                <label for="username">Username</label>
+                <input class="form-control" type="text" name="username" id="username" value="<?php echo escape(Input::get('username')); ?>" autocomplete="off">
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="field">
+                <label for="password">Choose a password</label>
+                <input class="form-control" type="password" name="password" id="password">
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="field">
+                <label for="password_again">Enter your password again</label>
+                <input class="form-control" type="password" name="password_again" id="password_again">
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="field">
+                <label for="name">Enter your name</label>
+                <input class="form-control" type="text" name="name" value="<?php echo escape(Input::get('name')); ?>" id="name">
+            </div>
+        </div>
+            <input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
+
+            <input type="submit" value="Register">
+        </form>
     </div>
-
-    <div class="field">
-        <label for="password">Choose a password</label>
-        <input type="password" name="password" id="password">
+</body>
+<footer>
+    <div class="footer">
+        <div>
+            <p>CRM 2018 Michael Schwartz</p>
+        </div>
     </div>
-
-    <div class="field">
-        <label for="password_again">Enter your password again</label>
-        <input type="password" name="password_again" id="password_again">
-    </div>
-
-    <div class="field">
-        <label for="name">Enter your name</label>
-        <input type="text" name="name" value="<?php echo escape(Input::get('name')); ?>" id="name">
-    </div>
-
-    <input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
-
-    <input type="submit" value="Register">
-</form>
+</footer>
+</html>
