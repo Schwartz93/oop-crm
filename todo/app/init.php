36f6b3e7d15a28b4677e4f69dbb218fe935a286a
Michael Schwartz <michael.schwartz@live.at>
2018-05-28 23:13:51 +0200

    Kleinigkeit

--- todo/app/init.php
+++ todo/app/init.php
@@ -1,7 +1,7 @@
 <?php 
 // Session wird gestartet
 session_start();
-// USER ID WIRD SIMULIERT!!!! ANBINDEN AN DIE USERS TABLE
+// FIXE SESSION ID! ANBINDEN AN DIE USERS TABLE
 $_SESSION['user_id'] = 1;
 // PDO Conn wird auf $db zugewiesen
 $db = new PDO('mysql:dbname=crm;host=127.0.0.1', 'root', 'root');
